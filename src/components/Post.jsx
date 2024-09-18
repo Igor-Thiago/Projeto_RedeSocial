c617013a7fdd44852c2a737a80de6d4fa6adac4f
Alladin-51 <igortls20@gmail.com>
2024-09-18 16:01:41 -0300

    Adicionando componente post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,7 +3,42 @@ export function Post(){
 
     return(
         <article className={styles.post}>
-            <p>Teste</p>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src='https://github.com/Igor-Thiago.png'/>
+                    <div className={styles.authorInfo}>
+                        <strong>Igor Thiago</strong>
+                        <span>Web developer</span>
+                    </div>
+                </div>
+                <time title="11 de Maio às 08:13h" dateTime="2024-09-18">Publicado a 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+                <p><a href=''>👉 jane.design/doctorcare</a></p>
+
+                <p>
+                    <a href=''>#novoprojeto </a>
+                    <a href=''>#nlw </a>
+                    <a href=''>#rocketseat</a>
+                </p>
+            </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea
+                    placeholder='Deixe um comentário'
+                />
+                
+                <footer>
+                    <button type='submit'>Comentar</button>
+                </footer>
+            </form>
         </article>
     )
 }
