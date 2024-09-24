48cca9581d76b2af6d0f874c032024188b53aa7a
Alladin-51 <igortls20@gmail.com>
2024-09-24 15:07:27 -0300

    Inserindo funcionalidade de likes infinitos

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,11 +1,16 @@
 import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
 import { Avatar } from './Avatar'
-
+import { useState } from 'react'
+  
 export function Comment ({ content, onDeleteComment }){
+    const [likeCount, setLikeCount] = useState(0);  // Varialvel contadora de likes e ua função para atualizar o número -> sempre importante inicializar um estado com o mesmo tipo da informação que vamos armazenar nele
 
     function handleDeleteComments(){
         onDeleteComment(content)
+    }
+    function handleLikeComment() {
+        setLikeCount(likeCount +1);
     }
 
     return(
@@ -29,9 +34,9 @@ export function Comment ({ content, onDeleteComment }){
 
 
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp />
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
