a1407da87095530946a221e80fe12442d11271b9
Alladin-51 <igortls20@gmail.com>
2024-09-25 15:08:37 -0300

    Aplicando propriedade de modo geral nos componentes post e avatar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Header } from './components/header.tsx';
-import { Post } from './components/Post.tsx';
+import { Post, PostType } from './components/Post.tsx';
 import { Sidebar } from './components/Sidebar.tsx';
 
 
 import './global.css'
 import styles from './App.module.css';
 
-const posts = [
+const posts: PostType[] = [
   {
     id: 1,
     author: {
@@ -57,9 +57,7 @@ export function App() {
             return (
               <Post 
                 key ={post.id}
-                author={post.author}
-                content={post.content}
-                publishedAt={post.publishedAt}
+                post={post}
               />
             )
           })}
--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,17 +1,19 @@
+import { ImgHTMLAttributes } from 'react';  // Isso importa uma classe já pronta do react com todas as propriedades possíveis que podem ter em uma imagem 
 import styles from './Avatar.module.css'
 
-interface AvatarProps {
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement>{
     hasBorder?: boolean;  // "?" indica que a propriedade não é obrigatória
     src: string;
 }
 
 
-export function Avatar({ hasBorder = true, src}: AvatarProps) {
+export function Avatar({ hasBorder = true, ...props}: AvatarProps) {  // Por conta da importação da primeira linha posso passar qualquer propriedade como parâmetro ao utilizar o "restOperator (...props)"
 
     return(
         <img 
             className={hasBorder ? styles.avatarWithBorder : styles.avatar} 
-            src={src} 
+            {...props} // Aqui eu declaro ele e assim quando for usar este componente
+                       // em qualuqer outro componente, posso passar qualquer pripriedade nele
         />
     )
 }
